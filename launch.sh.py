f56556ae7f97987432460102ef7672f79c8b4589
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2017-04-20 13:22:06 +0500

    Adding the new launch.sh generator file.
    Signed-off-by: Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>

---
+++ launch.sh.py
@@ -0,0 +1,49 @@
+from collections import namedtuple
+from optparse import OptionParser
+
+from downloader import output_ranges
+
+# Setting up the option parser
+parser = OptionParser()
+
+parser.add_option("-l", "--level", dest="level", type="str")
+parser.add_option("-p", "--part", dest="part", type="str")
+parser.add_option("-t", "--type", dest="type", type="str")
+parser.add_option("-c", "--chunk", dest="chunk", type="int", default=10000)
+
+(options, args) = parser.parse_args()
+
+Range = namedtuple("Range", ["level", "part", "type", "lowerbound", "upperbound"])
+PairedRange = namedtuple("PairedRange", ["level", "part", "type", "bounds", "numbounds"])
+if options.level:
+    level = [options.level]
+else:
+    level = ["SSC", "HSSC"]
+if options.part:
+    part = [options.part]
+else:
+    part = ["I", "II"]
+if options.type:
+    type_ = [options.type]
+else:
+    type_ = ["A", "S"]
+
+
+def return_pairs(lower_bound, upper_bound, chunk_size=10000):
+    lowers = list(range(lower_bound, upper_bound, chunk_size))
+    uppers = [num + chunk_size for num in lowers[:-1]] + [lowers[-1] + (upper_bound % chunk_size)]
+    return list(zip(lowers, uppers))
+
+
+ranges = [Range(l, p, t, *output_ranges(l, p, t)[:2]) for l in level for p in part for t in type_]
+
+pairs = [PairedRange(pair.level, pair.part, pair.type, return_pairs(pair.lowerbound, pair.upperbound),
+                     len(return_pairs(pair.lowerbound, pair.upperbound))) for pair in ranges]
+
+command_string = "docker run --detach -v /d/experiments/fbise/data:/app/fbise/data -v /d/experiments/fbise/logs:/app/logs --name worker{} localhost:5000/fbise /app/fbise/downloader.py -s {} -e {} -l {} -p {} -t {}\n"
+
+template = """
+#! /bin/bash
+
+{}
+"""
