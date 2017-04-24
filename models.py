93415b1092107997cedfd54b863d8299a9a1881a
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2017-04-24 14:56:17 +0500

    Adding the Student and Result class for further DB Cleaning.
    Signed-off-by: Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>

--- models.py
+++ models.py
@@ -8,3 +8,19 @@ class Record(db.Entity):
     rollno = orm.Required(str)
     html = orm.Required(str)
     error = orm.Required(bool)
+
+
+class Student(db.Entity):
+    rollno = orm.Required(int)
+    name = orm.Required(str)
+    father = orm.Required(str)
+    result = orm.Required(str)
+    institute = orm.Required(str)
+
+
+class Result(db.Entity):
+    student = orm.Required(Student)
+    subject = orm.Required(str)
+    part = orm.Required(str)
+    theory = orm.Required(int)
+    practical = orm.Required(int)
